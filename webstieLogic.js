2057ec2d676ca2240d334ed0c509334e29688a37
iEagle4303 <169015802+iEagle4303@users.noreply.github.com>
2024-06-29 19:32:25 -0500

    Initial commit

---
+++ webstieLogic.js
@@ -0,0 +1,19 @@
+export function UpdateTreeView(){
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+  console.log("click")
+}
+}
+export function UpdateNewTreeItem(item){
+  item.addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
+UpdateTreeView()
